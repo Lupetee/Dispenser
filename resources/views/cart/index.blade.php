f088a24d7a705bd622073a220337f96ecfd941e4
Lupetee <109496821+Lupetee@users.noreply.github.com>
2023-03-02 18:02:50 +0800

    Add files via upload

---
+++ resources/views/cart/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.admin')
+
+@section('title', 'Place Order')
+
+@section('content')
+    <div id="cart"></div>
+
+@endsection
---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      
+    </div>
+    <strong>&copy; <?php echo date('Y')?> - {{ config('app.name') }}</strong>
+  </footer>
